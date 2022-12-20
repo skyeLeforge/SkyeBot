33fca65a0253169823bb42ef8f3b39598ca25135
Skye <skyeleforge@gmail.com>
2022-12-19 20:52:37 -0500

    added sprites to the pokemon post

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { randomCard } = require("./commands/mtg.js");
 const { randomPokemon } = require("./commands/pokemon.js");
 const { pokemonSprite } = require("./commands/pokemonSprite.js");
-const { getRandomArrayElement } = require("./helpers/getRandomArrayElement.js");
 const { capitalizeEveryWord } = require("./helpers/captializeEveryWord.js");
 const owoify = require('owoify-js').default
 const Discord = require("discord.js");
@@ -41,6 +40,9 @@ bot.on('message', async (msg) => {
     msg.reply('wow, what a great post')
   }
 
+  if (command === 'test') {
+    msg.channel.send('foo')
+  }
 
   if (command === 'help') {
     msg.channel.send('Here are the commands currently aviliable:\
@@ -66,63 +68,80 @@ bot.on('message', async (msg) => {
   }
 
   if (command === 'pokemon') {
-    let text = 'Here is your perfect team:'
-    for (i = 0; i < 6; i++) {
-      randomPokemon().then(function (pokemon) {
-        pokemonSprite(pokemon.url).then(function (sprite) {
-          const capital = capitalizeEveryWord(pokemon.name)
-          text += `\n ${capital}`
-          text += `\n ${sprite}`
-        })
+    msg.channel.send('Calculating the perfect team, please wait...')
+    getTeam().then((res) => {
+      msg.channel.send(res.text)
+      msg.channel.send(res.sprites1)
+      msg.channel.send(res.sprites2)
+    })
+  }
 
-      })
+  if (command === 'rimg') {
+    msg.channel.send(`We're NOT doing this. ok`)
+  }
+
+  if (command === 'mtg') {
+    randomCard().then(function (cardData) {
+      let cardName = cardData.name
+      let cardIMG = cardData.image_uris.normal
+      msg.channel.send(`SkyeBot reccomends you put ${cardName} in your next mtg deck!`)
+      msg.channel.send(cardIMG)
+    })
+
+  }
+
+  if (command === 'owo') {
+    let toOwo = msg.content.slice(prefix.length + command.length).trim()
+    if (toOwo.length > 0)
+      msg.channel.send(owoify(toOwo))
+    else
+      msg.channel.send(`I'm a owo you`)
+  }
+
+  if (command === 'culture') {
+    msg.channel.send('```-play https://open.spotify.com/playlist/2bmhCh6qv4mbbqgfzVFIiE?si=5niRbaZ-TJGeR6z7GA6krw```')
+  }
+
+  if (command === "clear") {
+    //default deletes message itself plus previous
+    let num = 2;
+
+    //if argument is provided, we need to convert it from string to number
+    if (args[0]) {
+      //add 1 to delete clear command itself
+      num = parseInt(args[0]) + 1;
+      //bulk delete the messages
+      msg.channel.bulkDelete(num);
+      //notify channel of deleted messages
+      msg.channel.send(`deleted  ${args[0]} posts for you`);
+    } else {
+      console.log('0 messages to delete')
+      msg.channel.send('Ya didnt tell me how many messages to delete, stinky');
+    }
+
+  }
+})
+
+
+const getTeam = async () => {
+  let text = 'Here is your perfect team:'
+  let sprites1 = ''
+  let sprites2 = ''
+  for (i = 0; i < 6; i++) {
+    const pokemon = await randomPokemon()
+    const sprite = await pokemonSprite(pokemon.url)
+    // console.log(pokemon)
+    const capital = capitalizeEveryWord(pokemon.name)
+    text += `\n ${capital}`
+    if (i< 3){
+      sprites1 +=`\n ${sprite}`
+    } else {
+      sprites2 +=`\n ${sprite}`
 
     }
-    msg.channel.send(text)
+ 
+    // console.log(sprite)
   }
-
-if (command === 'rimg') {
-  msg.channel.send(`We're NOT doing this.`)
-}
-
-if (command === 'mtg') {
-  randomCard().then(function (cardData) {
-    let cardName = cardData.name
-    let cardIMG = cardData.image_uris.normal
-    msg.channel.send(`SkyeBot reccomends you put ${cardName} in your next mtg deck!`)
-    msg.channel.send(cardIMG)
-  })
+  return {text: text, sprites1: sprites1, sprites2: sprites2}
 
 }
-
-if (command === 'owo') {
-  let toOwo = msg.content.slice(prefix.length + command.length).trim()
-  if (toOwo.length > 0)
-    msg.channel.send(owoify(toOwo))
-  else
-    msg.channel.send(`I'm a owo you`)
-}
-
-if (command === 'culture') {
-  msg.channel.send('```-play https://open.spotify.com/playlist/2bmhCh6qv4mbbqgfzVFIiE?si=5niRbaZ-TJGeR6z7GA6krw```')
-}
-
-if (command === "clear") {
-  //default deletes message itself plus previous
-  let num = 2;
-
-  //if argument is provided, we need to convert it from string to number
-  if (args[0]) {
-    //add 1 to delete clear command itself
-    num = parseInt(args[0]) + 1;
-    //bulk delete the messages
-    msg.channel.bulkDelete(num);
-    //notify channel of deleted messages
-    msg.channel.send(`deleted  ${args[0]} posts for you`);
-  } else {
-    console.log('0 messages to delete')
-    msg.channel.send('Ya didnt tell me how many messages to delete, stinky');
-  }
-
-}
-})
