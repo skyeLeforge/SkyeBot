97e355aafd8782706a44387789d29fd3f82aa8a9
Skye <skyeleforge@gmail.com>
2022-12-19 20:30:34 -0500

    fixed timing bug with async functions

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const {randomCard} = require("./commands/mtg.js");
-const {randomPokemon} = require("./commands/pokemon.js");
-const {pokemonSprite} = require("./commands/pokemonSprite.js");
-const {getRandomArrayElement} = require("./helpers/getRandomArrayElement.js");
-const {capitalizeEveryWord} = require("./helpers/captializeEveryWord.js");
+const { randomCard } = require("./commands/mtg.js");
+const { randomPokemon } = require("./commands/pokemon.js");
+const { pokemonSprite } = require("./commands/pokemonSprite.js");
+const { getRandomArrayElement } = require("./helpers/getRandomArrayElement.js");
+const { capitalizeEveryWord } = require("./helpers/captializeEveryWord.js");
 const owoify = require('owoify-js').default
 const Discord = require("discord.js");
 
@@ -21,14 +21,14 @@ let prefix = ';'
 bot.on('message', async (msg) => {
   //if our message doesnt start with our defined prefix, dont go any further into function
   console.log(msg.content)
-  if(!msg.content.startsWith(prefix)) {
+  if (!msg.content.startsWith(prefix)) {
     console.log('no prefix')
     return
   }
-  
+
   //slices off prefix from our message, then trims extra whitespace, then returns our array of words from the message
   const args = msg.content.slice(prefix.length).trim().split(' ')
-  
+
   //splits off the first word from the array, which will be our command
   const command = args.shift().toLowerCase()
   //log the command
@@ -36,13 +36,13 @@ bot.on('message', async (msg) => {
   //log any arguments passed with a command
   console.log('args: ', args)
 
-  if(command === 'ego') {
+  if (command === 'ego') {
     msg.react("😀")
     msg.reply('wow, what a great post')
   }
 
 
-  if(command === 'help'){
+  if (command === 'help') {
     msg.channel.send('Here are the commands currently aviliable:\
                       \nhelp: Gives you help\
                       \nego: Boost Your Ego\
@@ -56,73 +56,73 @@ bot.on('message', async (msg) => {
                       \nrimg: no')
   }
 
-  if(command === 'prefix'){
+  if (command === 'prefix') {
     prefix = args[0]
     msg.channel.send(`Changed prefix to ${args[0]} `)
   }
 
-  if(command === 'stinky'){
-      msg.channel.send(msg.author.username +' is stinky')
+  if (command === 'stinky') {
+    msg.channel.send(msg.author.username + ' is stinky')
   }
 
-  if(command === 'pokemon') {
-    randomPokemon().then(function(pokemonList){
-      // generate 6 random pokemon
-      let text = 'Here is your perfect team:'
-      for(i=0; i<6; i++){
-        const pokemon = getRandomArrayElement(pokemonList)
-        const capital = capitalizeEveryWord(pokemon.name)
-        text += `\n ${capital}`
-        pokemonSprite(pokemon.url).then(function(sprite){
+  if (command === 'pokemon') {
+    let text = 'Here is your perfect team:'
+    for (i = 0; i < 6; i++) {
+      randomPokemon().then(function (pokemon) {
+        pokemonSprite(pokemon.url).then(function (sprite) {
+          const capital = capitalizeEveryWord(pokemon.name)
+          text += `\n ${capital}`
           text += `\n ${sprite}`
         })
-      }
-      msg.channel.send(text)
-    })
-  }
 
-  if(command === 'rimg'){
-    msg.channel.send(`We're NOT doing this.`)
-  }
+      })
 
-  if(command === 'mtg'){
-    randomCard().then(function(cardData){
-      let cardName = cardData.name
-      let cardIMG = cardData.image_uris.normal
-      msg.channel.send(`SkyeBot reccomends you put ${cardName} in your next mtg deck!`)
-      msg.channel.send(cardIMG)
-    })
-
-  }
-
-  if(command === 'owo'){
-    let toOwo = msg.content.slice(prefix.length + command.length).trim()
-    if(toOwo.length > 0)
-      msg.channel.send(owoify(toOwo))
-    else 
-      msg.channel.send(`I'm a owo you`)
-  }
-
-  if(command === 'culture'){
-    msg.channel.send('```-play https://open.spotify.com/playlist/2bmhCh6qv4mbbqgfzVFIiE?si=5niRbaZ-TJGeR6z7GA6krw```')
-  }
-
-  if (command === "clear") {
-    //default deletes message itself plus previous
-    let num = 2;
-    
-    //if argument is provided, we need to convert it from string to number
-    if (args[0]) {
-      //add 1 to delete clear command itself
-      num = parseInt(args[0]) + 1;
-       //bulk delete the messages
-       msg.channel.bulkDelete(num);
-       //notify channel of deleted messages
-       msg.channel.send(`deleted  ${args[0]} posts for you`);
-    }else{
-      console.log('0 messages to delete')
-      msg.channel.send('Ya didnt tell me how many messages to delete, stinky');
     }
-
+    msg.channel.send(text)
   }
+
+if (command === 'rimg') {
+  msg.channel.send(`We're NOT doing this.`)
+}
+
+if (command === 'mtg') {
+  randomCard().then(function (cardData) {
+    let cardName = cardData.name
+    let cardIMG = cardData.image_uris.normal
+    msg.channel.send(`SkyeBot reccomends you put ${cardName} in your next mtg deck!`)
+    msg.channel.send(cardIMG)
+  })
+
+}
+
+if (command === 'owo') {
+  let toOwo = msg.content.slice(prefix.length + command.length).trim()
+  if (toOwo.length > 0)
+    msg.channel.send(owoify(toOwo))
+  else
+    msg.channel.send(`I'm a owo you`)
+}
+
+if (command === 'culture') {
+  msg.channel.send('```-play https://open.spotify.com/playlist/2bmhCh6qv4mbbqgfzVFIiE?si=5niRbaZ-TJGeR6z7GA6krw```')
+}
+
+if (command === "clear") {
+  //default deletes message itself plus previous
+  let num = 2;
+
+  //if argument is provided, we need to convert it from string to number
+  if (args[0]) {
+    //add 1 to delete clear command itself
+    num = parseInt(args[0]) + 1;
+    //bulk delete the messages
+    msg.channel.bulkDelete(num);
+    //notify channel of deleted messages
+    msg.channel.send(`deleted  ${args[0]} posts for you`);
+  } else {
+    console.log('0 messages to delete')
+    msg.channel.send('Ya didnt tell me how many messages to delete, stinky');
+  }
+
+}
 })
